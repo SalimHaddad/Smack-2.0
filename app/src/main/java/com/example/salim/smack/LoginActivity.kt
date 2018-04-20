603135014d7907d1ccdec080492a59ee39dc8075
Salim <salim.elhaddad@lau.edu>
2018-04-20 15:22:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/salim/smack/LoginActivity.kt
@@ -0,0 +1,30 @@
+package com.example.salim.smack
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+
+
+
+    fun loginCreateUserBtnClicked(view: View)
+    {
+      val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
+    }
+
+
+    fun loginLoginBtnClicked(view: View)
+    {
+
+    }
+
+}
